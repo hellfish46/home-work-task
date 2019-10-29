24ad12211d0355d8c0b852fe6ccafdb53222cfe4
Anton Gritsun <hellfish46@i.ua>
2019-10-29 20:43:22 +0200

    lecture 4 homework start

--- src/main/java/com/hillel/lecture_4/BinaryToDecimalChecker.java
+++ src/main/java/com/hillel/lecture_4/BinaryToDecimalChecker.java
@@ -9,6 +9,30 @@ public class BinaryToDecimalChecker {
 
 //        TODO implements result
         int result = 0;
+        int x = value;
+        String resultInString = "";
+        System.out.println(x%2);
+
+        while(x != 1){
+            int ostatok = x%2;
+            x = Math.floorDiv(x , 2);
+
+            resultInString = resultInString + ostatok ;
+            System.out.println("x = " + x + ", ост = " + ostatok);
+
+        }
+        resultInString =  resultInString + x;
+        System.out.println(resultInString);
+        StringBuffer buffer = new StringBuffer(resultInString);
+        resultInString = buffer.reverse().toString();
+       // System.out.println(buffer);
+        result = Integer.parseInt(resultInString);
+
+
+
+
+
+
 
         return result;
     }
@@ -17,6 +41,12 @@ public class BinaryToDecimalChecker {
 
 //        TODO implements result
         int result = 0;
+        String valueString = "" + value;
+        int valueLength = valueString.length();
+        for(int x = 0; x<valueLength ;x++){
+           String latter = valueString.charAt(x);
+
+        }
 
         return result;
     }
